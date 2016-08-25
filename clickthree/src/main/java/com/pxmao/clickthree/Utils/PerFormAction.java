6b117a5df32b9f9acce4e12ae8a96e3c736f3ad4
Deepkeng <346755353@qq.com>
2016-08-25 15:49:04 +0800

    FerFormAction

--- clickthree/src/main/java/com/pxmao/clickthree/Utils/PerFormAction.java
+++ clickthree/src/main/java/com/pxmao/clickthree/Utils/PerFormAction.java
@@ -1,9 +1,18 @@
 package com.pxmao.clickthree.Utils;
 
+import android.util.Log;
+import android.view.accessibility.AccessibilityNodeInfo;
+
 /**
  * Created by psq on 2016/8/25
  */
 public class PerFormAction {
 
-
+    //执行点击
+   public static void performClickById(AccessibilityNodeInfo targetNode) {
+        Log.i("MyService", "通过ID执行点击");
+        if (targetNode.isClickable()) {
+            targetNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        }
+    }
 }
