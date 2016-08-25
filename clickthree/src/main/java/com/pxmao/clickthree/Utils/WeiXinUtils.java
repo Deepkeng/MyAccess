7e3db08b4bd85409657a8e19441211919ffc78a5
Deepkeng <346755353@qq.com>
2016-08-25 15:15:02 +0800

    WeiXinUtils

---
+++ clickthree/src/main/java/com/pxmao/clickthree/Utils/WeiXinUtils.java
@@ -0,0 +1,30 @@
+package com.pxmao.clickthree.Utils;
+
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.List;
+
+/**
+ * Created by psq on 2016/8/25
+ */
+public class WeiXinUtils {
+
+    /**
+     *
+     * @param rowNode 当前窗口跟节点getRootInActiveWindow()
+     * @return  返回微信当前聊天对象名字
+     */
+    public static String getCurrentCommunicateName(AccessibilityNodeInfo rowNode) {
+        CharSequence text;
+        if(rowNode != null){
+            List<AccessibilityNodeInfo> list = rowNode.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/ey");
+            if (list != null && !list.isEmpty()) {
+                text = list.get(0).getText();
+                return text.toString();
+            }
+        }
+        return null;
+
+    }
+
+}
